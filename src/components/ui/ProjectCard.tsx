d34ff84396f1dad8f08c91b1f22959c60858282b
NullerTy <dovispov4@gmail.com>
2024-11-13 10:13:16 +0100

    text ig

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -3,17 +3,15 @@ import Link from 'next/link'
 export const ProjectCard = ({
   title,
   description,
-  image,
   link,
 }: {
   title: string
   description: string
-  image: string
   link: string
 }) => {
   return (
     <div className='overflow-hidden rounded-lg bg-white shadow-lg'>
-      <img src={image} alt={title} className='h-48 w-full object-cover' />
+      
       <div className='p-6'>
         <h2 className='text-xl font-semibold text-gray-800'>{title}</h2>
         <p className='mt-2 text-gray-600'>{description}</p>
