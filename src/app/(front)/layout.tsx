e85346fbb74c29b36d7c411a0f7eaeb28d1805a4
NullerTy <dovispov4@gmail.com>
2024-11-13 10:58:32 +0100

    animation

--- src/app/(front)/layout.tsx
+++ src/app/(front)/layout.tsx
@@ -1,17 +1,33 @@
+'use client'
 import Footer from '@/components/layout/navs/footer'
 import { TopBar } from '@/components/layout/navs/top-bar'
+import { AnimatePresence, motion } from 'framer-motion'
+import { usePathname } from 'next/navigation'
 
 type Props = {
   children: React.ReactNode
 }
 
 const Layout: React.FC<Props> = ({ children }) => {
+  const pathname = usePathname()
+
   return (
     <>
-      <TopBar></TopBar>
-      {children}
-      <Footer></Footer>
+      <TopBar />
+      <AnimatePresence mode='wait' initial={false}>
+        <motion.div
+          key={pathname}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 20 }}
+          transition={{ duration: 0.3 }}
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
+      <Footer />
     </>
   )
 }
+
 export default Layout
--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -11,7 +11,6 @@ export const ProjectCard = ({
 }) => {
   return (
     <div className='overflow-hidden rounded-lg bg-white shadow-lg'>
-      
       <div className='p-6'>
         <h2 className='text-xl font-semibold text-gray-800'>{title}</h2>
         <p className='mt-2 text-gray-600'>{description}</p>
